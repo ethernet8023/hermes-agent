e75c2c2a1f46f9aa4d82b30c3a23f3cc6ec9d2e3
ethernet <arilotter@gmail.com>
2026-08-14 17:44:45 -0400

    chore: bump version to v0.22.0 (2026.8.14)

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -14,11 +14,11 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.21.1"
+__version__ = "0.22.0"
 __release_date__ = "2026.8.14"
 # This is the commit count at the release tag, used only by immutable Nix builds that do
 # not carry git history. `scripts/release.py` updates it for each new release.
-__release_rev_count__ = 22279
+__release_rev_count__ = 22280
 
 
 def _ensure_utf8():
